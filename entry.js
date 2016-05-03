de028544c94cf3e0dbeeb16f91a71dd34686b22a
James Nail <james.nail@lanetix.com>
2016-05-02 22:14:39 -0400

    initial empty web app

---
+++ entry.js
@@ -0,0 +1,5 @@
+import 'babel-polyfill'
+import './style.css'
+import content from './content'
+
+document.write(content)
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: ['babel-polyfill', './entry.js'],
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: 'style!css'
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015', 'stage-0']
+        }
+      }
+    ]
+  }
+}
